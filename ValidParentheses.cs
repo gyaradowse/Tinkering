bc67909b0990f7bc14f298eb03b613abe1fd78cc
iman izzat <imanizzatfarhan@gmail.com>
2022-05-17 14:05:23 +0800

    Added ValidParentheses Exercise

---
+++ ValidParentheses.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool IsValid(string s) {
+        var split = s.Split();
+        foreach (var string in split)
+        {
+            switch (string)
+            {
+                
+                default:
+            }
+        }
+    }
+}
